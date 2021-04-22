e77936eb8d967834aa0e3b5f7abd8e2ec5d5681a
Nahom <nahomish@gmail.com>
2021-04-22 14:39:14 +0200

    Reroute halvt klar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter as Router} from "react-router-dom"
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="Hoppas routingen funkar"></div>
+      <Route path="/" component={Home}/>
+      <Route path="/contact" component={Contact}/>
+
+    </Router>
   );
 }
 
