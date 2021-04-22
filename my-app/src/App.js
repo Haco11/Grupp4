226ba93b8a502c3937653e54503a33ef8928a24d
Nahom <nahomish@gmail.com>
2021-04-22 23:05:20 +0200

    Min och Carmens kombinerade kod, inklusive routingen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
-import {Route, BrowserRouter as Router} from "react-router-dom"
+import {Route, BrowserRouter as Router, Link} from "react-router-dom"
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact"
-
-function App() {
-  return (
-    <Router>
-      <div className="Hoppas routingen funkar"></div>
-      <Route path="/" component={Home}/>
-      <Route path="/contact" component={Contact}/>
-
-    </Router>
-  );
-}
-
-import './App.css';
+import Forum from "./Pages/Forum"
 import React from "react";
 import Likes from './Likes';
 import Threads from './Threads';
 import Comments from './Comments';
 import Category from './Category';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="routeContainer">
+        <nav>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/forum">Forum</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      <Route path="/" exact component={Home}/>
+      <Route path="/Forum" component={Forum}/>
+      <Route path="/contact" component={Contact}/>
+      </div>
+    </Router>
+  );
+}
+
 
 const App = () => 
    <div>
