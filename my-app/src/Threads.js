88d5b86267db073f93b872a5563937e85bebaadc
Carmen Iulia <carmeniulia@users.noreply.github.com>
2021-04-27 10:29:00 +0200

    Planning

--- my-app/src/Threads.js
+++ my-app/src/Threads.js
@@ -4,8 +4,11 @@ import Comments from './Comments';
 const Threads = () => (
 
 
-<div> <h2>Thread</h2>
-<Comments/>
+<div> <h2>Threads</h2>
+<p>Threads will be hidden and shown only when we choose a cateory</p>
+<p>Here we can see the threads from an specific category, they should be links and when clicked will show the specific thread and the comments that belong to the threads and how many likes have</p>
+<p>Below we should create a box to write a new comment for that thread</p>
+<p><Comments/></p>
 </div>
 
 );
