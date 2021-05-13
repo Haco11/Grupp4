27a7bce33642e284892800904e88a55fc193b6b2
Nahomish <nahomish@gmail.com>
2021-05-13 22:37:24 +0200

    Update Routing.js
    Uoodaterade routingen

--- my-app/src/Routing.js
+++ my-app/src/Routing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, BrowserRouter as Router, Link} from "react-router-dom"
+import {Route, BrowserRouter as Router, Link, HashRouter, Switch} from "react-router-dom"
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact"
 import Forum from "./Pages/Forum"
@@ -10,6 +10,7 @@ function App() {
     return (
       <Router>
         <div className="routeContainer">
+          <HashRouter>
           <nav>
             <ul>
                 <li><Link to="/Home">Home</Link></li>
@@ -17,9 +18,12 @@ function App() {
                 <li><Link to="/Contact">Contact</Link></li>
             </ul> 
           </nav>
+          <Switch>
         <Route path="/Home" exact component={Home}/>
         <Route path="/Forum" component={Forum}/>
         <Route path="/Contact" component={Contact}/>
+        </Switch>
+        </HashRouter>
         </div>
       </Router>
     );
